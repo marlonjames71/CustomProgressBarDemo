eda05e8c081524740b1b6d0760634ee0c915d1e4
Marlon <marlonr@hey.com>
2020-08-19 12:50:03 -0400

    Initial Commit

---
+++ CustomProgressBarDemo/CustomProgressBarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomProgressBarDemoApp.swift
+//  CustomProgressBarDemo
+//
+//  Created by Marlon Raskin on 8/19/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomProgressBarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
